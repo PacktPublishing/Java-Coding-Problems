7bd6ec4db2efda2b2b9425f0fae715b763693fb3
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-21 15:02:06 +0300

    update

--- Chapter_04/P80_ImplicitTypeCastingAndCodeMaintainability/src/modern/challenge/Main.java
+++ Chapter_04/P80_ImplicitTypeCastingAndCodeMaintainability/src/modern/challenge/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean payed = purchaseCart(1L);
-        System.out.println("Success: " + payed);
+        boolean paid = purchaseCart(1L);
+        System.out.println("Success: " + paid);
     }
 
     public static boolean purchaseCart(long customerId) {
 
         var price = ShoppingAddicted.fetchBestPriceAsInt(new String[0]);
-        var payed = ShoppingAddicted.debitCardAsInt(price);
+        var paid = ShoppingAddicted.debitCardAsInt(price);
 
-        return payed;
+        return paid;
     }
 }
