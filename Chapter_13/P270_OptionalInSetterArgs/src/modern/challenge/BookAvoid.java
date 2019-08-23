925dddf53d0cd40a3f28ae48c4a5912952b973ce
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-23 20:51:28 +0300

    update

--- Chapter_13/P270_OptionalInSetterArgs/src/modern/challenge/BookAvoid.java
+++ Chapter_13/P270_OptionalInSetterArgs/src/modern/challenge/BookAvoid.java
@@ -3,14 +3,18 @@ package modern.challenge;
 import java.util.Optional;
 
 public class BookAvoid {
-    
-    private Optional<String> isbn;      
+
+    private Optional<String> isbn;
 
     public Optional<String> getIsbn() {
         return isbn;
     }
 
     public void setIsbn(Optional<String> isbn) {
-        this.isbn = isbn;
-    }    
+        if (isbn == null) {
+            this.isbn = Optional.empty();
+        } else {
+            this.isbn = isbn;
+        }
+    }
 }
