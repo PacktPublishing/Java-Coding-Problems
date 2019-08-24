3924360fd9433d5300e4cf6dae9dc610c5bec0cb
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-24 07:33:18 +0300

    update

--- Chapter_13/P269_OptionalInConstructorsArgs/src/modern/challenge/BookAvoid.java
+++ Chapter_13/P269_OptionalInConstructorsArgs/src/modern/challenge/BookAvoid.java
@@ -6,11 +6,20 @@ import java.util.Optional;
 public class BookAvoid {
 
     private final String title;          // cannot be null
-    private final Optional<String> isbn; // optional field, thus may be empty
+    private final Optional<String> isbn; // optional field, cannot be null
 
     public BookAvoid(String title, Optional<String> isbn) {
         this.title = Objects.requireNonNull(title, () -> "Title cannot be null");
-        this.isbn = isbn;
+
+        /*
+        if (isbn == null) {
+            this.isbn = Optional.empty();
+        } else {
+            this.isbn = isbn;
+        }*/
+        
+        // or
+        this.isbn = Objects.requireNonNullElse(isbn, Optional.empty());
     }
 
     public String getTitle() {
