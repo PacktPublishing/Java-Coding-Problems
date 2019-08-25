3a7d00c46863da5c3118a232b1b9f28a81c5b4c8
AnghelLeonard <leoprivacy@yahoo.com>
2019-08-25 09:40:35 +0300

    update

--- Chapter_01/P01_CountDuplicateCharacters/src/modern/challenge/Strings.java
+++ Chapter_01/P01_CountDuplicateCharacters/src/modern/challenge/Strings.java
@@ -20,8 +20,8 @@ public final class Strings {
 
         Map<Character, Integer> result = new HashMap<>();                
 
+        // or use for(char ch: str.toCharArray()) { ... }
         for (int i = 0; i < str.length(); i++) {
-
             char ch = str.charAt(i);
 
             result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
